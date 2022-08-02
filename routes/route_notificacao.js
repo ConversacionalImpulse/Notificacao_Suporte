2ad130450be9863a82b5b46d8623175aa2cb7816
Daniel Sarmento <danielsarmento@users.noreply.github.com>
2022-08-02 13:14:56 -0300

    Mudança de post para get

--- routes/route_notificacao.js
+++ routes/route_notificacao.js
@@ -1,6 +1,6 @@
 const routes = require('express').Router();
 const controller = require('../controllers/controller_notificacao');
 
-routes.post('/notificacao', controller.notificacao)
+routes.get('/notificacao', controller.notificacao)
 
 module.exports = routes;
