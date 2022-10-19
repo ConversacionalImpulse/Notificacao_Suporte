d5111926676eaa8891e193c0d1891c8cfb4bf298
Daniel Sarmento <danielsarmento@users.noreply.github.com>
2022-10-19 16:20:51 -0300

    Ajuste

--- routes/route_notificacao.js
+++ routes/route_notificacao.js
@@ -1,11 +1,9 @@
 const routes = require('express').Router();
 const controller = require('../controllers/controller_notificacao');
-const controllerComercial = require('../controllers/controllerComercial');
 const controllerGrupoTop = require('../controllers/controllerGrupoTop.js')
 const controllerHorario = require('../controllers/controllerHorario.js')
 
 routes.post('/notificacao', controller.notificacao)
-routes.post('/notificacaoComercial', controllerComercial.notificacaoComercial)
 routes.post('/notificacaoGrupoTop', controllerGrupoTop.notificacao)
 routes.get('/diaEHora', controllerHorario.informacoes)
 
