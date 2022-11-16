5d5a78d845d5868d99995649521b961fbffb6078
Daniel Sarmento <danielsarmento@users.noreply.github.com>
2022-11-16 16:20:31 -0300

    ajuste vercel

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 
-const routes = require('./routes/route_notificacao');
+const routes = require('./src/routes/route_notificacao');
 const PORT = process.env.PORT;
 
 app.use(express.urlencoded({ extended: true }));
