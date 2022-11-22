446c963174aa464b770d979cd511a15185d1c17b
Daniel Sarmento <danielsarmento@users.noreply.github.com>
2022-11-22 17:42:09 -0300

    Cadastro de Clientes Impulse

---
+++ src/controllers/controller_clientes_impulse.js
@@ -0,0 +1,46 @@
+const { PrismaClient } = require('@prisma/client')
+const prisma = new PrismaClient()
+
+exports.cadastrar_cliente = async (req, res) =>{
+    const { nome, email, telefone, empresa} = req.body;
+    const verifica_cadastro = await prisma.clientes.findMany({
+        where: {
+            telefone: telefone
+        }
+    })
+
+    if(verifica_cadastro.length > 0){
+        res.json({cadastrado: false, msg: "Cliente já cadastrado no sistema", dados: verifica_cadastro})
+    } else {
+        const cliente = await prisma.clientes.create({
+            data: {
+                nome: nome,
+                email: email,
+                telefone: telefone,
+                empresa: empresa
+            }
+        })
+        res.json({cadastrado: true, msg: "Cliente cadastrado com sucesso", dados: cliente})
+    }
+}
+
+exports.buscar_cliente = async (req, res) => {
+    const {telefone} = req.body;
+    const verifica_cadastro = await prisma.clientes.findMany({
+        where: {
+            telefone: telefone
+        }
+    })
+    
+    if(verifica_cadastro > 0){
+        res.json({
+            cadastrado: true,
+            verifica_cadastro
+        })
+    } else {
+        res.json({
+            cadastrado: false
+        })
+    }
+    
+}
---
+++ src/routes/routes.js
@@ -0,0 +1,19 @@
+const routes = require('express').Router();
+const controller_notificacao_impulse = require('../controllers/controller_notificacao_impulse');
+const controller_notificacao_grupoTop = require('../controllers/controller_notificacao_grupoTop.js');
+const controller_horario = require('../controllers/controller_horario.js');
+const controller_clientes_impulse = require('../controllers/controller_clientes_impulse.js')
+
+//Rotas de Notificação
+routes.post('/notificacao', controller_notificacao_impulse.notificacao);
+routes.post('/notificacaoGrupoTop', controller_notificacao_grupoTop.notificacao);
+
+//Rota de Data
+routes.get('/diaEHora', controller_horario.informacoes);
+
+//Rota de Cadastros do Suporte da Impulse
+routes.post('/cadastroClienteImpulse', controller_clientes_impulse.cadastrar_cliente)
+routes.post('/buscarClienteImpulse', controller_clientes_impulse.buscar_cliente)
+
+
+module.exports = routes;
