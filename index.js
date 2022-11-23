77682843cd4b7e324c2de3fc2ee151fd4957e783
Daniel Sarmento <danielsarmento@users.noreply.github.com>
2022-11-23 13:01:08 -0300

    ajuste de schema

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ const PORT = process.env.PORT;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(routes)
+app.use(routes);
 
 app.listen( PORT, () => {
     console.log(`Servidor rodando na porta ${PORT}`)
-})
+});
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -3,7 +3,7 @@ const controller_notificacao_impulse = require('../controllers/controller_notifi
 const controller_notificacao_grupoTop = require('../controllers/controller_notificacao_grupoTop.js');
 const controller_horario = require('../controllers/controller_horario.js');
 const controller_clientes_impulse = require('../controllers/controller_clientes_impulse.js');
-const controller_usuarios_impulse = require('../controllers/controller_usuarios_impulse.js');
+
 
 //Rotas de Notificação
 routes.post('/notificacao', controller_notificacao_impulse.notificacao);
@@ -15,8 +15,5 @@ routes.get('/diaEHora', controller_horario.informacoes);
 //Rota de Cadastros do Suporte da Impulse
 routes.post('/cadastroClienteImpulse', controller_clientes_impulse.cadastrar_cliente)
 routes.post('/buscarClienteImpulse', controller_clientes_impulse.buscar_cliente)
-
-//Rotass de Usuarios Impulse
-routes.post('/cadastrarUsuarioImpulse', controller_usuarios_impulse.cadastrar_usuarios)
 
 module.exports = routes;
