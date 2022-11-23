d58c23624a783885e6ec386650fac7eca55f95d6
Daniel Sarmento <danielsarmento@users.noreply.github.com>
2022-11-23 17:56:30 -0300

    teste

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser')
+
 
 const routes = require('./src/routes/routes');
 const PORT = process.env.PORT;
 
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(routes);
