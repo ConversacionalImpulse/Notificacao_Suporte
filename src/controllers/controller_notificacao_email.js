7c3715ac4f3c08c3d4369e2dc91202cbedd6a309
Daniel Sarmento <danielsarmento@users.noreply.github.com>
2023-01-03 16:09:31 -0300

    ajuste email impulse

--- src/controllers/controller_notificacao_email.js
+++ src/controllers/controller_notificacao_email.js
@@ -2,7 +2,7 @@ const nodemailer = require('nodemailer');
 
 exports.notificacao = async (req, res) => {
     const {cliente, protocol, email} = req.body
-    const senha = 'hyatdcyavvnagzof';
+    const crypt = 'hyatdcyavvnagzof';
 
     const transport = nodemailer.createTransport({
         host: 'smtp.gmail.com',
@@ -10,20 +10,20 @@ exports.notificacao = async (req, res) => {
         secure: true,
         auth:{
             user: 'daniel.impulseb@gmail.com',
-            pass: senha
+            pass: crypt
         }
     });
     
     try {
         await transport.sendMail({
-            from: 'Daniel Sarmento <daniel.impulseb@gmail.com>',
+            from: 'Impulse <daniel.impulseb@gmail.com>',
             to: [email],
             subject: `Impulse: caso ${protocol}`,
             html: `
             <h1>Olá, ${cliente}!</h1> 
-            <p>O caso <b>${protocol}</b>, referente ao assunto <b>Atendimento Chat: WHATSAPP - ${cliente}</b> está resolvido.</p> 
-            <p>Caso entenda que essa demanda ainda não está resolvida, por favor, não hesite em nos contatar.<p> 
-            <p>Orientamos que responda este e-mail em até <b>3 dias úteis</b> ou entrar em contato pelo nosso WhatsApp e informar a nosso atendente o número desse caso para que o chamado seja reaberto. Caso contrário, este chamado será concluído de forma automática.</p> 
+            <p>O caso <b>${protocol}</b>, referente ao assunto <b>Atendimento Chat: WHATSAPP - ${cliente}</b> está em resolução.</p> 
+            <p>Caso deseja acrescentar novas informações a essa demanda, por favor, não hesite em nos contatar.<p> 
+            <p>Orientamos que responda este e-mail em até <b>3 dias úteis</b> ou entrar em contato pelo nosso WhatsApp e informar a nosso atendente o número desse caso para que o chamado seja identificado.</p> 
             <p>Se precisar de ajuda com outro assunto, por favor, entre em contato através do nosso <b>WhatsApp +55 83 9908-8426</b></p> 
             <p>Estamos aqui para te ajudar de <b>segunda à sexta-feira, das 8h às 18h (exceto feriados)</b>.</p> 
             <h5>Atenciosamente, Equipe de Atendimento Impulse</h5>`,
